a8c51f855f7c33c90f5899c44d9d9f2fad3aa4f4
dluksa20 <dluksa20@gmail.com>
2025-10-26 01:51:04 +0100

    search-bar added

--- src/token_frontend/src/components/Header/Header.jsx
+++ src/token_frontend/src/components/Header/Header.jsx
@@ -1,47 +1,60 @@
 import React from "react";
-import "./Header.css"
-import { Link } from "react-router-dom";
-
-
+import "./Header.css";
+import { Link, useLocation } from "react-router-dom";
+import { BsSearch } from 'react-icons/bs';
 // Navigation routes
 const navLinks = {
-  home: {
-    title: "HOME",
-    route: "/"
-  },
-  wallet: {
-    title: "WALLET",
-    route: "/wallet"
-  }
-}
-
-
+  home: { title: "Home", route: "/" },
+  wallet: { title: "Wallet", route: "/wallet" },
+};
 
 function Header() {
+  const location = useLocation();
+
   return (
-    <header>
-
+    <header className="header">
       <div className="header-container">
-
-
+        {/* Logo */}
         <div className="nav-logo">
-          <Link to ="/">
-              <img src="./public/assets/wallet.svg" alt="wallet logo" />
-              <h1>ZeRo</h1>
+          <Link to="/" className="logo-link">
+            <img src="/assets/wallet.svg" alt="wallet logo" />
+            <h1>Ze<span className="accent">Ro</span></h1>
           </Link>
         </div>
 
 
-        <div className="nav-links">
-          {
-            Object.keys(navLinks).map((key) => (
-                <Link key={key} to={navLinks[key].route} className="nav-link">
-                  {navLinks[key].title}
-                </Link>
-            ))
-          }
+
+        {/* Search bar */}
+        <div className="search-form-component">
+
+          <div className="search-form-input">
+            <form action="">
+              <input type="search" id="header-search" placeholder="Search" aria-label="Search field" />
+            </form>
+          </div>
+
+          <div className="seearch-form-button">
+              <button type="submit" aria-label="Submit Search" >
+                <BsSearch />
+              </button>
+          </div>
+
         </div>
 
+        {/* Navigation */}
+        <nav className="nav-links">
+          {Object.keys(navLinks).map((key) => (
+            <Link
+              key={key}
+              to={navLinks[key].route}
+              className={`nav-link ${
+                location.pathname === navLinks[key].route ? "active" : ""
+              }`}
+            >
+              {navLinks[key].title}
+            </Link>
+          ))}
+        </nav>
       </div>
     </header>
   );
